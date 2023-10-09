9d7133fdcdef2dab80d6d6fb914a77337d0e9c19
Kirill <kirillkuznecov5754@gmail.com>
2023-10-09 20:33:20 +0300

    1

--- triangle.py
+++ triangle.py
@@ -1,12 +1,9 @@
 def area(a, h):
-<<<<<<< HEAD
     '''Принимaет числа a, h возвращает площадь треугольника'''
     return a * h / 2
 
 def perimeter(a, b, c):
     '''Принимaет числа a, b, с возвращает периметр треугольника'''
-=======
     return a * h / 2
 def perimeter(a, b, c):
->>>>>>> f7930115d81dd8891830bf83b585cf0c9072ff9a
     return a + b + c
